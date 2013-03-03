8199adf1bc60bb949f1590ed07a7aa67e8c27fc5
abrkn <a@abrkn.com>
2013-03-03 18:53:43 +0700

    move secret to Drop ctor; add createOffer

--- test/drop.js
+++ test/drop.js
@@ -1,16 +1,23 @@
 var Drop = require('../lib/drop')
 , expect = require('expect.js')
+, andyAddr = 'rG4muW7MgLqFV1VgzJ7sJ6ADr3xAAPujVi'
+, dropAddr = 'ra2Mv6bbtoBtQt1AqPd232MN7Yf4zo1QCX'
+, defaults = {
+    secrets: {
+        'ra2Mv6bbtoBtQt1AqPd232MN7Yf4zo1QCX': 'ssd5TpRAshGAF2Mg9GxZNSpKXaDr8'
+    }
+}
 
 describe('drop', function() {
     describe('constructor', function() {
         it('has a default uri', function() {
-            var r = new Drop()
+            var r = new Drop(defaults)
             expect(r.opts.uri).to.eql('wss://s1.ripple.com:51233')
             r.socket.terminate()
         })
 
         it('connects on startup', function() {
-            var r = new Drop()
+            var r = new Drop(defaults)
             expect(r.socket.readyState == 'connecting')
             r.socket.terminate()
         })
@@ -26,7 +33,7 @@ describe('drop', function() {
 
     describe('send', function() {
         it('queues when not connected', function() {
-            var r = new Drop()
+            var r = new Drop(defaults)
             r.send({})
             expect(r.queue.length).to.be(1)
             r.socket.terminate()
@@ -45,14 +52,14 @@ describe('drop', function() {
 
         it('resolves the promise', function(done) {
             this.timeout(10e3)
-            var r = new Drop()
+            var r = new Drop(defaults)
             r.send({ command: 'ping' })
             .fin(done)
         })
 
         it('rejects on error', function(done) {
             this.timeout(10e3)
-            var r = new Drop()
+            var r = new Drop(defaults)
             r.send({ command: 'doesnotexist' })
             .fail(function(err) {
                 expect(err.name).to.be('unknownCmd')
@@ -64,7 +71,7 @@ describe('drop', function() {
     describe('subscribe', function() {
         it('defaults to server', function() {
             this.timeout(10e3)
-            var r = new Drop()
+            var r = new Drop(defaults)
             r.subscribe(function() { })
             expect(r.queue[0].message.streams).to.eql(['server'])
             r.socket.terminate()
@@ -73,7 +80,7 @@ describe('drop', function() {
         it('eventually receives a ledgerClosed when subing to ledger', function(done) {
             this.timeout(120e3)
 
-            var r = new Drop()
+            var r = new Drop(defaults)
 
             // the .done will cause any exception to be thrown
             r.subscribe('ledger', function(type, result) {
@@ -90,10 +97,10 @@ describe('drop', function() {
         it('can fetch andys account details', function(done) {
             this.timeout(10e3)
 
-            var r = new Drop()
-            r.account('rG4muW7MgLqFV1VgzJ7sJ6ADr3xAAPujVi')
+            var r = new Drop(defaults)
+            r.account(dropAddr)
             .then(function(result) {
-                expect(result.account_data.Account).to.be('rG4muW7MgLqFV1VgzJ7sJ6ADr3xAAPujVi')
+                expect(result.account_data.Account).to.be(dropAddr)
                 r.socket.terminate()
                 done()
             })
@@ -105,11 +112,10 @@ describe('drop', function() {
         it('fails when attempting to send to self', function(done) {
             this.timeout(10e3)
 
-            var r = new Drop()
+            var r = new Drop(defaults)
             r.payment(
-                'ssd5TpRAshGAF2Mg9GxZNSpKXaDr8',
-                'ra2Mv6bbtoBtQt1AqPd232MN7Yf4zo1QCX',
-                'ra2Mv6bbtoBtQt1AqPd232MN7Yf4zo1QCX',
+                dropAddr,
+                dropAddr,
                 1e8)
             .fail(function(err) {
                 expect(err.message).to.match(/to self/)
@@ -120,11 +126,10 @@ describe('drop', function() {
         it('can send xrp to andy', function(done) {
             this.timeout(10e3)
 
-            var r = new Drop()
+            var r = new Drop(defaults)
             r.payment(
-                'ssd5TpRAshGAF2Mg9GxZNSpKXaDr8',
-                'ra2Mv6bbtoBtQt1AqPd232MN7Yf4zo1QCX',
-                'rG4muW7MgLqFV1VgzJ7sJ6ADr3xAAPujVi',
+                dropAddr,
+                andyAddr,
                 1)
             .then(function(result) {
                 expect(result.tx_json.hash).to.be.a('string')
@@ -138,8 +143,8 @@ describe('drop', function() {
     describe('accountLines', function() {
         it('can fetch lines for andy', function(done) {
             this.timeout(10e3)
-            var r = new Drop()
-            r.accountLines('rG4muW7MgLqFV1VgzJ7sJ6ADr3xAAPujVi')
+            var r = new Drop(defaults)
+            r.accountLines(dropAddr)
             .then(function(result) {
                 expect(result).to.be.an('array')
                 r.socket.terminate()
@@ -152,8 +157,8 @@ describe('drop', function() {
     describe('accountTransactions', function() {
         it('can fetch andys transactions', function(done) {
             this.timeout(10e3)
-            var r = new Drop()
-            r.accountTransactions('rG4muW7MgLqFV1VgzJ7sJ6ADr3xAAPujVi', 0, 403707)
+            var r = new Drop(defaults)
+            r.accountTransactions(dropAddr, 0, 403707)
             .then(function(result) {
                 expect(result).to.be.an('array')
                 r.socket.terminate()
@@ -166,14 +171,60 @@ describe('drop', function() {
     describe('transaction', function() {
         it('can fetch a known tx', function(done) {
             this.timeout(10e3)
-            var r = new Drop()
+            var r = new Drop(defaults)
             r.transaction('2E5D2E21E9D40DEAA2BD1C322A43974A32289A94E01A4B1F5BABFF98A78E5914')
             .then(function(tx) {
-                expect(tx.Account).to.be('ra2Mv6bbtoBtQt1AqPd232MN7Yf4zo1QCX')
+                expect(tx.Account).to.be(dropAddr)
                 r.socket.terminate()
                 done()
             })
             .fail(done)
+        })
+    })
+
+    // getting internal errors
+    /*
+    describe('subscribeBook', function() {
+        it('can fetch xrp/btc book', function(done) {
+            this.timeout(10e3)
+            var r = new Drop(defaults)
+            r.subscribeBook('XRP', 'BTC', null, dropAddr, function(result) {
+                console.log(result)
+            })
+            .done()
+        })
+    })
+    */
+
+    describe('createOffer', function() {
+        it('fails when trying BTC/stamp to XRP because unfunded', function(done) {
+            this.timeout(10e3)
+            var r = new Drop(defaults)
+            r.createOffer(
+                dropAddr,
+                '1 BTC/rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B',
+                '10000000000000 XRP'
+            )
+            .fail(function(err) {
+                expect(err.message).to.match(/balance to fund/)
+                done()
+            })
+            .done()
+        })
+
+        it('is upset when trying 1 XRP for 2 XRP', function(done) {
+            this.timeout(10e3)
+            var r = new Drop(defaults)
+            r.createOffer(
+                dropAddr,
+                '1 XRP',
+                '2 XRP'
+            )
+            .fail(function(err) {
+                expect(err.message).to.match(/bad offer/i)
+                done()
+            })
+            .done()
         })
     })
 })

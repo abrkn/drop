8e2b0a581c6e3953b9f5f2ab4d2f6fe1679ab045
abrkn <a@abrkn.com>
2013-03-06 00:24:45 +0700

    more work on amounts

--- test/Amount.js
+++ test/Amount.js
@@ -12,9 +12,16 @@ describe('Amount', function() {
 
         it('supports single number arg', function() {
             var a = new Amount(100)
-            expect(a.value).to.be('100')
+            expect(a.value).to.be(100 / 1e6 + '')
             expect(a.currency).to.not.be.ok()
             expect(a.issuer).to.not.be.ok()
+        })
+
+        it('supports issuer in string', function() {
+            var a = new Amount('0.1 BTC/rnipVEZ2hi6qh7BedivFR13rXHYdpzhKfc')
+            expect(a.value).to.be('0.1')
+            expect(a.currency).to.be('BTC')
+            expect(a.issuer).to.be('rnipVEZ2hi6qh7BedivFR13rXHYdpzhKfc')
         })
 
         it('accepts full string', function() {
@@ -26,13 +33,13 @@ describe('Amount', function() {
 
         it('accepts integer arg', function() {
             var a = new Amount(42)
-            expect(a.value).to.be('42')
+            expect(a.value).to.be(42 / 1e6 + '')
         })
     })
 
     describe('toString', function() {
         it('makes with only number for XRP', function() {
-            var a = new Amount(1, 'XRP')
+            var a = new Amount('1')
             expect(a.toString()).to.be('1')
         })
     })
